e941d3f27a0ffc84b7bea0361666431bbb32b48d
Andre Barros <andrebarros7201@gmail.com>
2024-05-18 20:38:41 +0100

    create form div and display div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Test");
+import "./style.css";
+
+//Get body and create the form div and display div
+const body = document.querySelector("body");
+
+const divForm = document.createElement("div");
+divForm.classList.add("div-form");
+body.appendChild(divForm);
+
+const divDisplay = document.createElement("div");
+divDisplay.classList.add("div-form");
+body.appendChild(divDisplay);
